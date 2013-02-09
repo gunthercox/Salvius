56ea3ecb4b008cbaa086a3d393fde9d2c1cdf8ff
gunthercox <gunthercx@gmail.com>
2013-02-09 12:02:37 -0500

    added binary to hexidecimal unit conversion

--- interaction/src/convert.java
+++ interaction/src/convert.java
@@ -62,4 +62,63 @@ public class convert {
 		return (fahrenheit - 32) * 5 / 9;
 	}
 	
+	// BINARY TO HEX
+	public static String binary_to_hex(String binary) {
+		
+		// BREAK THE BINARY VALUE INTO QUARTETS
+		String[] quartet = new String[3];
+		quartet[0] = binary.substring(0, 4);
+		quartet[1] = binary.substring(4, 8);
+		quartet[2] = binary.substring(8, 12);
+		
+		String hexidecimal = "";
+		String hex;
+		
+		// SELECT A 
+		for (int i = 0; i < quartet.length; i++) {
+			switch (quartet[i]) {
+            case "0000": hex = "0";
+            break;
+            case "0001": hex = "1";
+            break;
+            case "0010": hex = "2";
+            break;
+            case "0011": hex = "3";
+            break;
+            case "0100": hex = "4";
+            break;
+            case "0101": hex = "5";
+            break;
+            case "0110": hex = "6";
+            break;
+            case "0111": hex = "7";
+            break;
+            case "1000": hex = "8";
+            break;
+            case "1001": hex = "9";
+            break;
+            case "1010": hex = "A";
+            break;
+            case "1011": hex = "B";
+            break;
+            case "1100": hex = "C";
+            break;
+            case "1101": hex = "D";
+            break;
+            case "1110": hex = "E";
+            break;
+            case "1111": hex = "F";
+            break;
+            default: hex = "";
+            break;
+	        }
+			
+			// CONCATINATE THE QUARTETS
+			hexidecimal += hex;
+			
+		}
+		
+		return hexidecimal;
+	}
+	
 }
