d3250e70cb640bc58b6aa736e11de51974da9f5b
gunthercox <gunthercx@gmail.com>
2013-02-21 08:06:26 -0500

    Created class tabs

---
+++ interface/src/web_interface/Robot_Head.java
@@ -0,0 +1,15 @@
+package web_interface;
+
+/**
+ * Servlet implementation class Robot_Head
+ */
+public class Robot_Head {
+	
+	// TAB CONTENT HTML
+	public static String tab = "<ul class='dropdown-menu' role='menu'>" +
+	"<p class='text-center'>Click a point on the view screen to center it.</p></ul>";
+
+	// TAB EVENT JQUERY
+	public static String script = "";
+
+}
---
+++ interface/src/web_interface/Robot_Lights.java
@@ -0,0 +1,15 @@
+package web_interface;
+
+public class Robot_Lights {
+
+	// TAB CONTENT HTML
+	public static String tab = "<ul class='dropdown-menu' role='menu'>" +
+		        				"<div class='btn-group' data-toggle='buttons-checkbox'>" +
+		        				"<button type='button' class='btn btn-large btn-danger'>IR</button>" +
+		        				"<button type='button' class='btn btn-large btn-inverse'>UV</button>" +
+		        				"</div></ul>";
+
+	// TAB EVENT JQUERY
+	public static String script = "";
+	
+}
