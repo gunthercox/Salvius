efd36dafa1682e23c7988b5fd1fd91cab359a081
gunthercox <gunthercx@gmail.com>
2013-02-21 20:40:59 -0500

    added shutdown alert and camera target dot

--- interface/src/web_interface/Power.java
+++ interface/src/web_interface/Power.java
@@ -7,12 +7,14 @@ public class Power {
 								"<div class='btn-group' data-toggle='buttons-radio'>" +
 								"<button type='button' class='btn btn-large active'>" +
 								"<i class='icon-circle'></i></button>" +
-								"<button type='button' class='btn btn-large'><i class='icon-circle-blank'></i></button>" +
+								"<button type='button' id='off' class='btn btn-large'><i class='icon-circle-blank'></i></button>" +
 								"</div>" +
 								"<p class='text-center'>Battery: " + "33.05%" + "</p>" +
 								"</ul>";
 
 	// TAB EVENT JQUERY
-	public static String script = "";
+	public static String script = "$('#off').click(function() {" +
+								"return confirm('Are you sure you want to deactivate the robot?');" +
+								"});";
 	
 }
