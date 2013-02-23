172f6f5897989d2497008c22fe82d78a68376180
gunthercox <gunthercx@gmail.com>
2013-02-23 08:15:07 -0500

    added no-cache headers

--- interface/src/web_interface/salvius_gui.java
+++ interface/src/web_interface/salvius_gui.java
@@ -44,11 +44,16 @@ public class salvius_gui extends HttpServlet {
 		//Voice robotVoice = new Voice("kevin16");
 		//robotVoice.say("testing 1 2 3");
 		
+		// PREVENT PAGE FROM BEING CACHED
+		response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+		response.setHeader("Pragma", "no-cache");
+		response.setDateHeader("Expires", 0);
+		
 		// GET HEADERS
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         
-        out.println(Utilities.head("Interface") +
+        out.print(Utilities.head("Interface") +
         		"<body><div class='circle'>");
         
         // CREATE THE PRIMARY TAB RING
@@ -57,100 +62,108 @@ public class salvius_gui extends HttpServlet {
         	// GIVE THE TAB AN ID NUMBER
         	tab[i][2] = Integer.toString(i);
         	
-    		out.println(Utilities.tab(tab[i][1], tab.length, i, tab[i][2], tab[i][0], ""));
+    		out.print(Utilities.tab(tab[i][1], tab.length, i, tab[i][2], tab[i][0], ""));
     		
 			// THIS WILL BECOME A CASE SELECT
 			// OR, --> FOR X IN TAB <--
 	
 			// HEAD CONTROL
 			if (i == 0) {
-				out.println(Robot_Head.tab);
+				out.print(Robot_Head.tab);
 			}
     		
 			// OPERATING MODE
 			if (i == 1) {					        
-		        out.println(Control_Mode.tab);
+		        out.print(Control_Mode.tab);
 			}
 
 		    // LIGHTS
 			if (i == 2) {
-		        out.println(Robot_Lights.tab);
+		        out.print(Robot_Lights.tab);
 			}
 			
 			// TEXT TO SPEECH
 			if (i == 3) {
-				out.println("<ul class='dropdown-menu well tts' style='-webkit-transform:rotate(" + ((360 / tab.length) * (-i)) + "deg);'>" +
+				out.print("<ul class='dropdown-menu well tts' style='-webkit-transform:rotate(" + ((360 / tab.length) * (-i)) + "deg);'>" +
 							"<div class='row'></div><div class='row span4'>" +
 							"<input type='text' class='span3' placeholder='Enter text to speak'>");
-				out.println(Utilities.mediaControler());
-				out.println("</div></ul>");
+				out.print(Utilities.mediaControler());
+				out.print("</div></ul>");
 			}
 			
 			// HAND-WRITING
 			if (i == 4) {
-				out.println("<ul class='dropdown-menu well txt' style='-webkit-transform:rotate(" + ((360 / tab.length) * (-i)) + "deg);'>" +
+				out.print("<ul class='dropdown-menu well txt' style='-webkit-transform:rotate(" + ((360 / tab.length) * (-i)) + "deg);'>" +
 							"<div class='row span4'>" +
 							"<input type='text' class='span3' placeholder='Enter text to write'>");
-				out.println(Utilities.mediaControler());
-				out.println("</div></ul>");
+				out.print(Utilities.mediaControler());
+				out.print("</div></ul>");
 			}
 			
 			// SENSOR READINGS
 			if (i == 7) {
-				out.println("<ul class='dropdown-menu sensor' style='-webkit-transform:rotate(" + ((360 / tab.length) * (-i)) + "deg);'>");
-				out.println(Utilities.table(sensorData));
-				out.println("</ul>");
+				out.print("<ul class='dropdown-menu sensor' style='-webkit-transform:rotate(" + ((360 / tab.length) * (-i)) + "deg);'>");
+				out.print(Utilities.table(sensorData));
+				out.print("</ul>");
 			}
 			
 		    // POWER
 			if (i == 8) {
-		        out.println(Power.tab);
+		        out.print(Power.tab);
 			}
     					
-    		out.println("</div>");
+    		out.print("</div>");
         	}
         
         // CAMERA FEED & EXPAND-ALL / COLLAPSE-ALL BUTTONS
-		out.println("<img id='camera' src='./img/img.jpg' />" +
-					"<div id='dot'></div>" +
-					"<button class='toggle btn btn-primary icon-folder-close' id='toggle' data-toggle='button'></button>" +
-					"</div>");
+		out.print("<div class='transparent'></div>" +
+				"<img id='camera' src='./img/img.jpg' />" +
+				"<div id='dot'></div>" +
+				"<button class='toggle btn btn-primary icon-folder-close' id='toggle' data-toggle='button'></button>" +
+				"</div>");
 		
-		out.println("<script>");
+		out.print("<script>");
 		
-		out.println(Power.script);
+		out.print(Power.script);
 					
-		out.println("$('#camera').click(function(e) {" +
-					"if ($('.rotate').hasClass('open')) {" +
-					"$('#dot').addClass('dot icon-screenshot');" +
-					"$('.dot').css({'top':e.pageY, 'left':e.pageX});" +
-					"} else {" +
-					"$('#dot').removeClass('dot icon-screenshot');" +
-					"}" +
-					"});" +
-					
-					"$('.toggle').each(function() {" +
-					"$(this).click(function() {" +
-					"$(this).toggleClass('btn-success icon-folder-open');" +
-					"if ($('.rotate').hasClass('open')) {" +
-					"$('.rotate').removeClass('open');" +
-					"} else {" +
-					"$('.rotate').addClass('open');" +
-					"}" +
-					"});" +
-					"});" +
-					
-					"$('.tab').click(function() {" +
-					"if ($(this).parent('div').hasClass('open')) {" +
-					"$(this).parent('div').removeClass('open');" +
-					"} else {" +
-					"$(this).parent('div').addClass('open');" +
-					"}" +
-					"});" +
-					
-					"</script>");
+		out.print("$('#camera').click(function(e) {" +
+				"if ($('.rotate').hasClass('open')) {" +
+				"$('#dot').addClass('dot icon-screenshot');" +
+				"$('.dot').css({'top':e.pageY, 'left':e.pageX});" +
+				"} else {" +
+				"$('#dot').removeClass('dot icon-screenshot');" +
+				"}" +
+				"});" +
+				
+				"$('.toggle').each(function() {" +
+				"$(this).click(function() {" +
+				"$(this).toggleClass('btn-success icon-folder-open');" +
+				"if ($('.rotate').hasClass('open')) {" +
+				"$('.rotate').removeClass('open');" +
+				"} else {" +
+				"$('.rotate').addClass('open');" +
+				"}" +
+				"});" +
+				"});" +
+				
+				"$('.tab').click(function() {" +
+				"if ($(this).parent('div').hasClass('open')) {" +
+				"$(this).parent('div').removeClass('open');" +
+				"} else {" +
+				"$(this).parent('div').addClass('open');" +
+				"}" +
+				"});" +
+				
+    			"$(document).ready(function() {" +
+				"window.setInterval('reload();', 1000);" +
+	 			"});" +
+				"function reload(){" +
+					"$('#camera').attr('src', './img/img.jpg');" +
+				"}" +
+				
+				"</script>");
 		
-        out.println("</body></html>");
+        out.print("</body></html>");
         
 		// CLOSE STREAM
         out.close();
