62d34a41906805bb1d406046009d99456b1b2449
gunthercox <gunthercx@gmail.com>
2013-03-10 13:20:55 -0400

    css and toggle feature

--- gui/src/com/Utilities.java
+++ gui/src/com/Utilities.java
@@ -12,22 +12,46 @@ public class Utilities {
 				"<link rel='stylesheet' type='text/css' href='./css/bootstrap.css' />" +
 				"<link rel='stylesheet' href='css/normalize.css' />" +
 				"<link rel='stylesheet' href='css/foundation.css' />" +
-				"<script src='js/vendor/foundation.min.js'></script>" +
+				"<script src='js/foundation.min.js'></script>" +
+				"<script src='js/foundation/foundation.alerts.js'></script>" +
+				"<script src='js/vendor/jquery.js'></script>" +
+				"<script src='js/vendor/zepto.js'></script>" +
 				"<script src='js/vendor/custom.modernizr.js'></script>");
 	}
+	
+	// CREATE TABS: TITLE | ICON
+	public static String[][] tab = {
+		{"Head", "icon-eye-open"},
+	    {"Settings", "icon-cog"},
+	    {"Lights", "icon-lightbulb"},
+	    {"Speech", "icon-volume-up"},
+	    {"Write", "icon-pencil"},
+	    {"Tools", "icon-wrench"},
+	    {"Conversation", "icon-globe"},
+	    {"Sensor readings", "icon-bar-chart"},
+	    {"Power", "icon-off"}
+	    };
+	
+	// ARRAY OF ARDUINO INPUT VALUES
+	public static String[][] sensorData = {
+		{"ID", "Sensor", "Value"},
+		{"~1", "PIR", "51.5 W"},
+		{"~2", "Light", "61.2 Lm"},
+		{"A0", "Sound", "0.2 dB"}
+	};
+	
+	public static String[] tabular = new String[tab.length];
 	
 	// TABS
 	public static String tab(String master_state, String minor_state, String icon, int totalTabs, int id, String title, String content) {
 		
 		String open = "";
-		String value = "0";
 		
-		if ("1".equals(minor_state)) { value = "0"; } else { value = "1"; }
-		if ("1".equals(master_state) || "1".equals(minor_state)) { open = "open"; } else { open = " "; }
+		if ("1".equals(minor_state) || "1".equals(master_state)) { tabular[id] = "0"; open = "open"; } else { tabular[id] = "1"; open = " "; }
 		
 		return("<div class='rotate " + open + " t" + Integer.toString(id) + "'>" +
 				"<button name='tab" + id + "' class='tab btn btn-inverse dropdown' title='" + title + "' " +
-				"value='" + value + "'" +
+				"value='" + tabular[id] + "'" +
 				"data-toggle='dropdown'>" +
 				"<i class='" + icon + "'></i></button>");
 	}
@@ -59,31 +83,7 @@ public class Utilities {
 		
 		sb.append("</tbody></table>");
 		
-		return sb.toString();
-		
+		return sb.toString();	
 	}
-	
-	// CREATE TABS: TITLE | ICON
-	public static String[][] tab = {
-		{"Head", "icon-eye-open"},
-	    {"Settings", "icon-cog"},
-	    {"Lights", "icon-lightbulb"},
-	    {"Speech", "icon-volume-up"},
-	    {"Write", "icon-pencil"},
-	    {"Tools", "icon-wrench"},
-	    {"Conversation", "icon-globe"},
-	    {"Sensor readings", "icon-bar-chart"},
-	    {"Power", "icon-off"}
-	    };
-	
-	// ARRAY OF ARDUINO INPUT VALUES
-	public static String[][] sensorData = {
-		{"ID", "Sensor", "Value"},
-		{"~1", "PIR", "51.5 W"},
-		{"~2", "Light", "61.2 Lm"},
-		{"A0", "Sound", "0.2 dB"}
-	};
-	
-	public static String[] tabular = new String[tab.length];
 
 }
