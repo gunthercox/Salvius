820906d0b2ae9261e5e1693d336cbc043cfc35fc
gunthercox <gunthercx@gmail.com>
2013-07-06 11:48:25 -0400

    removed these files

--- src/cox/salvius/gui.java
+++ src/cox/salvius/gui.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/servlet")
+@WebServlet("/")
 public class gui extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
