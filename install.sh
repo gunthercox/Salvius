fb3b4b55ae4709df3219f5d785a8fc6743e059b3
gunthercox <gunthercx@gmail.com>
2013-07-11 18:24:03 -0400

    improved install so that setup sets user permission

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 printf "Moving files into current directory...\r"
 
-sudo mv -f ./Salvius/. ./www
+sudo mv -f * ./Salvius/[A-Za-z0-9]* ../
+
+printf "Removing empty download directory"
+
+sudo rm -r Salvius
+
+printf "Giving permissions to current user"
+
+sudo chown $USER -R ../www
 
 printf "Updating package cache...\r"
 
