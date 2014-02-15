8913550497de3ea25558453744c95b53ba076cd8
gunthercox <gunthercx@gmail.com>
2014-02-15 18:02:08 -0500

    set up serial output to test api post

--- arduino/api/settings.h
+++ arduino/api/settings.h
@@ -3,3 +3,6 @@ static uint8_t mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x00, 0x8F };
 
 // CHANGE THIS TO MATCH YOUR HOST NETWORK
 static uint8_t ip[] = { 192, 168, 1, 177 };
+
+// USED FOR WEB SERVER
+#define PREFIX ""
