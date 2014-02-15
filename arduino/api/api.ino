e8bc97db577ea99197519f22423510112e719971
gunthercox <gunthercx@gmail.com>
2014-02-15 11:18:30 -0500

    added i2c scanning to the web interface

--- arduino/api/api.ino
+++ arduino/api/api.ino
@@ -24,18 +24,13 @@
 #include "WebServer.h"
 #include <Wire.h>
 
+#include "settings.h"
+
 // no-cost stream operator as described at 
 // http://sundial.org/arduino/?page_id=119
 template<class T>
 inline Print &operator <<(Print &obj, T arg)
 { obj.print(arg); return obj; }
-
-
-// CHANGE THIS TO YOUR OWN UNIQUE VALUE
-static uint8_t mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x00, 0x8F };
-
-// CHANGE THIS TO MATCH YOUR HOST NETWORK
-static uint8_t ip[] = { 192, 168, 1, 177 };
 
 #define PREFIX ""
 
@@ -52,7 +47,6 @@ void jsonCmd(WebServer &server, WebServer::ConnectionType type, char *url_tail, 
     return;
   }
 
-  //server.httpSuccess(false, "application/json");
   server.httpSuccess("application/json");
   
   if (type == WebServer::HEAD)
@@ -84,11 +78,6 @@ void outputPins(WebServer &server, WebServer::ConnectionType type, bool addContr
     "<html>"
     "<head>"
     "<title>Arduino Web Server</title>"
-    "<style type=\"text/css\">"
-    "BODY { font-family: sans-serif }"
-    "H1 { font-size: 14pt; text-decoration: underline }"
-    "P  { font-size: 10pt; }"
-    "</style>"
     "</head>"
     "<body>";
 
@@ -136,6 +125,55 @@ void outputPins(WebServer &server, WebServer::ConnectionType type, bool addContr
   server << "</body></html>";
 }
 
+void outputI2c(WebServer &server, WebServer::ConnectionType type)
+{
+  byte error, address;
+  int nDevices;
+  
+    P(htmlHead) =
+    "<html>"
+    "<head>"
+    "<title>Arduino Web Server</title>"
+    "</head>"
+    "<body>";
+
+  server.httpSuccess();
+  server.printP(htmlHead);
+  
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) 
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      server << "I2C device found at address 0x";
+      if (address<16) 
+        server << "0";
+      server << address;
+      server << HEX;
+
+      nDevices++;
+    }
+    else if (error==4) 
+    {
+      server << "Unknow error at address 0x";
+      if (address<16) 
+        server << "0";
+      server << address;
+      server << HEX;
+    }    
+  }
+  if (nDevices == 0)
+    server << "No I2C devices found";
+  else
+    server << "done";
+}
+
 void formCmd(WebServer &server, WebServer::ConnectionType type, char *url_tail, bool tail_complete)
 {
   if (type == WebServer::POST)
@@ -159,6 +197,11 @@ void formCmd(WebServer &server, WebServer::ConnectionType type, char *url_tail, 
     outputPins(server, type, true);
 }
 
+void i2cCmd(WebServer &server, WebServer::ConnectionType type, char *url_tail, bool tail_complete)
+{
+    outputI2c(server, type);
+}
+
 void defaultCmd(WebServer &server, WebServer::ConnectionType type, char *url_tail, bool tail_complete)
 {
   outputPins(server, type, false);  
@@ -177,86 +220,11 @@ void setup()
   webserver.setDefaultCommand(&defaultCmd);
   webserver.addCommand("json", &jsonCmd);
   webserver.addCommand("form", &formCmd);
+  webserver.addCommand("i2c", &i2cCmd);
   
   // I2C Scanning
   Wire.begin();
   Serial.begin(9600);
-  Serial.println("\nI2C Scanner");
-}
-
-void i2cScan()
-{
- // --------------------------------------
-// i2c_scanner
-//
-// Version 1
-//    This program (or code that looks like it)
-//    can be found in many places.
-//    For example on the Arduino.cc forum.
-//    The original author is not know.
-// Version 2, Juni 2012, Using Arduino 1.0.1
-//     Adapted to be as simple as possible by Arduino.cc user Krodal
-// Version 3, Feb 26  2013
-//    V3 by louarnold
-// Version 4, March 3, 2013, Using Arduino 1.0.3
-//    by Arduino.cc user Krodal.
-//    Changes by louarnold removed.
-//    Scanning addresses changed from 0...127 to 1...119,
-//    according to the i2c scanner by Nick Gammon
-//    http://www.gammon.com.au/forum/?id=10896
-// Version 5, March 28, 2013
-//    As version 4, but address scans now to 127.
-//    A sensor seems to use address 120.
-// 
-//
-// This sketch tests the standard 7-bit addresses
-// Devices with higher bit address might not be seen properly.
-//
-
-/*
-The sketch shows the 7-bit addresses of the found devices as 
-hexadecimal values. That value can be used for the "Wire.begin" 
-function which uses the 7-bit address. Some datasheets use the 
-8-bit address and some example sketches use decimal addresses.
-*/
-  byte error, address;
-  int nDevices;
-
-  Serial.println("Scanning...");
-
-  nDevices = 0;
-  for(address = 1; address < 127; address++ ) 
-  {
-    // The i2c_scanner uses the return value of
-    // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-
-    if (error == 0)
-    {
-      Serial.print("I2C device found at address 0x");
-      if (address<16) 
-        Serial.print("0");
-      Serial.print(address,HEX);
-      Serial.println("  !");
-
-      nDevices++;
-    }
-    else if (error==4) 
-    {
-      Serial.print("Unknow error at address 0x");
-      if (address<16) 
-        Serial.print("0");
-      Serial.println(address,HEX);
-    }    
-  }
-  if (nDevices == 0)
-    Serial.println("No I2C devices found\n");
-  else
-    Serial.println("done\n");
-
-  delay(5000);           // wait 5 seconds for next scan 
 }
 
 void loop()
@@ -264,6 +232,6 @@ void loop()
   // process incoming connections one at a time forever
   webserver.processConnection();
 
-  // if you wanted to do other work based on a connecton, it would go here
-  //i2cScan();
+  // start additional work here
+  //i2cCmd();
 }
---
+++ arduino/api/settings.h
@@ -0,0 +1,5 @@
+// CHANGE THIS TO THE UNIQUE VALUE OF YOUR ETHERNET SHIELD
+static uint8_t mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x00, 0x8F };
+
+// CHANGE THIS TO MATCH YOUR HOST NETWORK
+static uint8_t ip[] = { 192, 168, 1, 177 };
