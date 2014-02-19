fa71536a0fe90552ac0bfc8fcb4bf6511be012fe
gunthercox <gunthercx@gmail.com>
2014-02-19 14:28:13 -0500

    combined slave test and tts

--- arduino/tts/tts.ino
+++ arduino/tts/tts.ino
@@ -7,28 +7,17 @@
  to an RC filter then through an amplifier to the speaker.
  */
 
+#include <Wire.h>
 #include "TTS.h"
 
-// Media pins
-#define ledPin 13       // digital pin 13                          
+// digital pin 13
+#define ledPin 13
 
-// Variables
 char text [50];
-boolean state=0;
+boolean state = 0;
 
-TTS text2speech;  // speech output is digital pin 10
-
-void setup() { 
-  //media
-  pinMode(ledPin, OUTPUT); 
-}
-
-void loop(){
-  state = !state;
-  digitalWrite(ledPin, state);
-  Test_Speech();
-  delay(1000);
-}  
+// speech output is digital pin 10
+TTS text2speech;
 
 void Test_Speech() {
   // higher values = lower voice pitch
@@ -38,4 +27,43 @@ void Test_Speech() {
   text2speech.say(text);
 }
 
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany) {
+  String x = String(Wire.read());    // receive byte as an integer
+    Serial.println(x);
+}
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  
+  Serial.begin(9600);
+  Serial.println("Slave online");
+  
+  // join i2c bus with address #4
+  Wire.begin(4);
+  
+  // register event
+  Wire.onReceive(receiveEvent);
+}
+
+void loop() {
+  
+  // slave may send less than requested
+  while(Wire.available())
+  {
+    // receive a byte as character
+    char c = Wire.read();
+    Serial.print(c);
+    delay(1000);
+  }
+  
+  while(Wire.available() == false)
+  {
+  state = !state;
+  digitalWrite(ledPin, state);
+  Test_Speech();
+  delay(1000);
+  }
+}
 
