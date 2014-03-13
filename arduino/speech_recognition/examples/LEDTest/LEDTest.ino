98b776cc41e619882d516169d2ae225ff4bf7ca1
gunthercox <gunthercx@gmail.com>
2014-03-13 19:47:23 -0400

    Added uSpeech library for testing speech recognition

---
+++ arduino/speech_recognition/examples/LEDTest/LEDTest.ino
@@ -0,0 +1,46 @@
+
+#include "uspeech.h"
+#define led 13
+signal voice(A0);
+String collvoice;
+char prev;
+boolean newline=false;
+int sum = 0;
+void setup(){
+  voice.f_enabled = true;
+  voice.minVolume = 1500;
+  voice.fconstant = 400;
+  voice.econstant = 1;
+  voice.aconstant = 2;
+  voice.vconstant = 3;
+  voice.shconstant = 4;
+  voice.calibrate();
+  Serial.begin(9600);
+  pinMode(led, OUTPUT); 
+}
+
+void loop(){
+    voice.sample();
+    char p = voice.getPhoneme();
+    
+    if (p != prev) {
+      Serial.println(p);
+    }
+      
+    if(p!=' ') {
+      if((p=='e')) {
+          newline = false;
+      } else {
+          
+          newline = true;
+      }
+    } else {
+      if(newline) {
+        digitalWrite(led, LOW);
+      }
+      else {
+        digitalWrite(led, HIGH);
+      }
+    }
+    prev = p;
+}
