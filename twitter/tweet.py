d71d911904c6ce8eff698a20c3512f860d328620
gunthercox <gunthercx@gmail.com>
2014-03-24 10:30:43 -0400

    created dictionary for twitter settings

--- twitter/tweet.py
+++ twitter/tweet.py
@@ -4,11 +4,13 @@ from random import choice
 
 import settings
 
+debug = settings.DEBUG
+
 auth = OAuth(
-    consumer_key = settings.CONSUMER_KEY,
-    consumer_secret = settings.CONSUMER_SECRET,
-    token = settings.TOKEN,
-    token_secret = settings.TOKEN_SECRET
+    consumer_key = settings.TWITTER["CONSUMER_KEY"],
+    consumer_secret = settings.TWITTER["CONSUMER_SECRET"],
+    token = settings.TWITTER["TOKEN"],
+    token_secret = settings.TWITTER["TOKEN_SECRET"]
 )
 
 t = Twitter(auth=auth)
@@ -19,10 +21,14 @@ user = t.account.verify_credentials()["screen_name"]
 # Get the list of robots
 robots = t.lists.members(owner_screen_name=user, slug="Robots")["users"]
 
-# Tweet one random message to the next robot every hour
+# Tweet one random message to the next robot every hour if not in debug mode
+
 for robot in robots:
-    #sleep(3600-time()%3600)
     message = " " + choice(open("messages.txt").readlines())
     msg = ("@" + robot["screen_name"] + message).strip("\n")
-    #t.statuses.update(status=msg)
-    print(msg)
+
+    if debug == True:
+        print(msg)
+    else:
+        sleep(3600-time()%3600)
+        t.statuses.update(status=msg)
