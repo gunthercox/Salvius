5f9a1f13b5aada6d84a5fa0b4717a761d0ed1f8d
Gunther Cox <gunthercx@gmail.com>
2014-07-05 20:43:33 -0400

    Added leg serializers

--- robot/leg/ankle.py
+++ robot/leg/ankle.py
@@ -5,3 +5,8 @@ class Ankle(object):
     def __init__(self):
         self.angle = 0
         self.tilt = 0
+
+
+class AnkleSerializer(Serializer):
+    angle = fields.Integer()
+    tilt = fields.Integer()
