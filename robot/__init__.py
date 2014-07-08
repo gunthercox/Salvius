22fdd249f6795b91f24c3f96a15e3f4751263b92
Gunther Cox <gunthercx@gmail.com>
2014-07-07 21:07:32 -0400

    Created default robot constructor

--- robot/__init__.py
+++ robot/__init__.py
@@ -1,5 +1,11 @@
 from marshmallow import Serializer, fields
-from .body import Body, BodySerializer
+from robot.body import Body, BodySerializer
+
+from robot.arm import Arm
+from robot.arm.shoulder import Shoulder
+from robot.arm.elbow import Elbow
+from robot.arm.wrist import Wrist
+from robot.arm.hand import Hand, Finger
 
 
 class Robot(object):
@@ -11,6 +17,56 @@ class Robot(object):
     @property
     def body(self):
         return self._body
+
+    def default(self):
+        """
+        Creates a basic humanoid robot
+        """
+        # Create left arm
+        left_arm = Arm()
+        self.body.add_arm(left_arm)
+
+        left_shoulder = Shoulder()
+        left_arm.set_shoulder(left_shoulder)
+
+        leftElbow = Elbow()
+        left_arm.set_elbow(leftElbow)
+
+        left_wrist = Wrist()
+        left_arm.set_wrist(left_wrist)
+
+        left_hand = Hand()
+        left_arm.set_hand(left_hand)
+
+        left_fingers = [Finger(), Finger(), Finger(), Finger()]
+        left_thumb = Finger()
+        for finger in left_fingers:
+            left_hand.add_finger(finger)
+
+        left_hand.set_thumb(left_thumb)
+
+        # Create right arm
+        right_arm = Arm()
+        self.body.add_arm(right_arm)
+
+        right_shoulder = Shoulder()
+        right_arm.set_shoulder(right_shoulder)
+
+        leftElbow = Elbow()
+        right_arm.set_elbow(leftElbow)
+
+        right_wrist = Wrist()
+        right_arm.set_wrist(right_wrist)
+
+        right_hand = Hand()
+        right_arm.set_hand(right_hand)
+
+        right_fingers = [Finger(), Finger(), Finger(), Finger()]
+        right_thumb = Finger()
+        for finger in right_fingers:
+            right_hand.add_finger(finger)
+
+        right_hand.set_thumb(right_thumb)
 
 
 class RobotSerializer(Serializer):
