b58461bd2ef48abfcaa5ed9fb51c37377a5a4fb3
Gunther Cox <gunthercx@gmail.com>
2014-08-10 07:53:55 -0400

    Added util module

---
+++ humanoid/util.py
@@ -0,0 +1,10 @@
+def degrees_to_seconds(degrees, speed):
+    """
+    Converts a number of degrees to an equivilant durration of seconds.
+    Requires the speed (revolutions per minute) that the object will rotate at.
+    """
+
+    # Convert revolutions per minute to revolutions per second
+    rps = speed * 60
+
+    # TODO: return seconds
