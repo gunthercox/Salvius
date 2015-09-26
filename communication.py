00c893855e1ccf2f1887fdd56c19c48778194663
Gunther Cox <gunthercx@gmail.com>
2015-09-26 17:40:13 -0400

    Migrated project to use Zorg robotics framework.

---
+++ communication.py
@@ -0,0 +1,35 @@
+from zorg.adaptor import Adaptor
+from zorg.driver import Driver
+from chatterbot import ChatBot
+
+
+class Conversation(Adaptor):
+
+    def __init__(self, options):
+        super(Conversation, self).__init__(options)
+
+        self.chatbot = ChatBot(
+            "Salvius",
+            io_adapter="chatterbot.adapters.io.JsonAdapter"
+        )
+
+    def train_if_required(self):
+        """
+        Train the current chatterbot instance
+        if it has not been trained yet.
+        """
+        # TODO
+
+
+class ApiDriver(Driver):
+
+    def __init__(self, options, connection):
+        super(ApiDriver, self).__init__(options, connection)
+
+        self.commands += [
+            "get_response",
+        ]
+
+    def get_response(self, input_value):       
+        return self.connection.chatbot.get_response(input_value)
+
--- salvius.py
+++ salvius.py
@@ -1,71 +1,47 @@
-from chatterbot import ChatBot
-from chatterbot.apis.twitter import Twitter
-from chatterbot.apis.github import GitHub
-from jsondb.db import Database
-from flask import Flask
-from flask.ext.cors import CORS
+import time
+import zorg
 
-from humanoid import api
-from humanoid.neck import Neck
-from humanoid.torso import Torso
-from humanoid.arm.shoulder import Shoulder
-from humanoid.arm.elbow import Elbow
-from humanoid.arm.wrist import Wrist
-from humanoid.arm.hand import Finger
-from humanoid.arm.hand import Thumb
-from humanoid.leg.hip import Hip
-from humanoid.leg.knee import Knee
-from humanoid.leg.ankle import Ankle
+from communication import Conversation, ApiDriver
 
-settings_available = False
+def test():
+    #print "x"
+    time.sleep(1)
 
-try:
-    import settings
-    settings_available = True
-except ImportError:
-    pass
+def work(salvius):
+    while True:
+        #print salvius.camera_one.get_url()
+        test()
 
-# Create flask app
-app = Flask(__name__, static_folder="static", static_url_path="")
-cors = CORS(app)
+        time.sleep(1)
 
-app.add_url_rule("/connect/github/", view_func=api.GitHubConnectView.as_view("connect_github"))
-app.add_url_rule("/connect/twitter/", view_func=api.TwitterAuthorizedView.as_view("connect_twitter"))
+robot = zorg.robot({
+    "name": "Salvius",
+    "connections": {
+        "camera": {
+            "adaptor": "zorg_network_camera.Camera",
+        },
+        "chatterbot": {
+            "adaptor": "communication.Conversation"
+        }
+        #"raspberry_pi": {
+            # "adaptor": "zorg-raspi.RasPi",
+        #},
+    },
+    "devices": {
+        "camera_one": {
+            "connection": "camera",
+            "driver": "zorg_network_camera.Feed",
+            "url": "http://192.168.1.6/image.jpg",
+        },
+        "communication": {
+            "connection": "chatterbot",
+            "driver": "communication.ApiDriver",
+        },
+    },
+    "work": work,
+})
 
-app.add_url_rule("/arms/<string:arm_name>/shoulder/", view_func=Shoulder.as_view("shoulder"))
-app.add_url_rule("/arms/<string:arm_name>/elbow/", view_func=Elbow.as_view("elbow"))
-app.add_url_rule("/arms/<string:arm_name>/wrist/", view_func=Wrist.as_view("wrist"))
-app.add_url_rule("/arms/<string:arm_name>/finger/", view_func=Finger.as_view("finger"))
-app.add_url_rule("/arms/<string:arm_name>/thumb/", view_func=Thumb.as_view("thumb"))
+api = zorg.api("zorg.api.Http", {})
 
-app.add_url_rule("/legs/<string:leg_name>/hip/", view_func=Hip.as_view("hip"))
-app.add_url_rule("/legs/<string:leg_name>/knee/", view_func=Knee.as_view("knee"))
-app.add_url_rule("/legs/<string:leg_name>/ankle/", view_func=Ankle.as_view("ankle"))
-
-app.add_url_rule("/neck/", view_func=Neck.as_view("neck"))
-app.add_url_rule("/torso/", view_func=Torso.as_view("torso"))
-
-app.add_url_rule("/api/terminate/",view_func=api.Terminate.as_view("terminate"))
-app.add_url_rule("/api/speech/", view_func=api.Speech.as_view("speech"))
-app.add_url_rule("/api/writing/", view_func=api.Writing.as_view("writing"))
-app.add_url_rule("/api/chat/", view_func=api.Chat.as_view("chat"))
-app.add_url_rule("/api/status/", view_func=api.Status.as_view("status"))
-app.add_url_rule("/api/device_ports/", view_func=api.DevicePorts.as_view("device_ports"))
-
-if __name__ == "__main__":
-    app.config["CHATBOT"] = ChatBot()
-    app.config["DATABASE"] = Database("settings.db")
-    app.config["DEBUG"] = True
-    app.config["SECRET_KEY"] = "development"
-
-    if settings_available:
-        if hasattr(settings, "GITHUB"):
-            app.config["GITHUB"] = GitHub(settings.GITHUB)
-        if hasattr(settings, "TWITTER"):
-            app.config["TWITTER"] = Twitter(settings.TWITTER)
-        if hasattr(settings, "GOOGLE"):
-            app.config["GOOGLE"] = settings.GOOGLE
-        if hasattr(settings, "DISQUS"):
-            app.config["DISQUS"] = settings.DISQUS
-
-    app.run(host="0.0.0.0", port=8000)
+robot.start()
+api.start()
