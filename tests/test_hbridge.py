f7ca5a40593718c754a09e7436732e7f31795e49
Gunther Cox <gunthercx@gmail.com>
2016-10-05 11:19:18 -0400

    Change structure of h-bridge module.

--- tests/test_hbridge.py
+++ tests/test_hbridge.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from zorg.test import MockAdaptor
-from salvius.hbridge import HBridge
+from salvius.hbridge import RelayHBridge, ServoHBridge
 
 
 class HBridgeTestCase(TestCase):
@@ -9,11 +9,49 @@ class HBridgeTestCase(TestCase):
         super(HBridgeTestCase, self).setUp()
 
         self.connection = MockAdaptor({
-            'methods': ['hbridge_write']
+            'methods': ['servo_write', 'digital_write']
         })
         self.options = {}
 
-        self.driver = HBridge(self.options, self.connection)
+
+class RelayHBridgeTestCase(HBridgeTestCase):
+
+    def setUp(self):
+        super(RelayHBridgeTestCase, self).setUp()
+        self.options['pins'] = [1, 2, 3, 4] 
+        self.driver = RelayHBridge(self.options, self.connection)
+
+    def test_command_method_exists(self):
+        """
+        Check that each command listed has a corresponding
+        method on the driver class.
+        """
+        for command in self.driver.commands:
+            self.assertIn(command, dir(self.driver))
+
+    def test_four_pins_not_given(self):
+        self.options['pins'] = []
+        with self.assertRaises(RelayHBridge.RelayException):
+            RelayHBridge(self.options, self.connection)
+
+    def test_turn_off(self):
+        self.driver.turn_off()
+        self.assertEqual(self.driver.state, 0)
+
+    def test_rotate_clockwise(self):
+        value = self.driver.rotate_clockwise()
+        self.assertEqual(self.driver.state, 1)
+
+    def test_rotate_counterclockwise(self):
+        value = self.driver.rotate_counterclockwise()
+        self.assertEqual(self.driver.state, -1)
+
+
+class ServoHBridgeTestCase(HBridgeTestCase):
+
+    def setUp(self):
+        super(ServoHBridgeTestCase, self).setUp()
+        self.driver = ServoHBridge(self.options, self.connection)
 
     def test_command_method_exists(self):
         """
